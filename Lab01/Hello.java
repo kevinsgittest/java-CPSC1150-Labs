6ab5b775e877e2357a1d0208ec8f67f51f9d46ba
kburts <kevin.burton2@gmail.com>
2014-05-11 21:42:27 -0700

    Testing repo. Added lab1

---
+++ Lab01/Hello.java
@@ -0,0 +1,29 @@
+/*Program Name: Hello
+ *Author: Kevin
+ *Date: May 11 2014
+ *Course: CPSC1150-1
+ *Instructor: H.Darbandi
+ *Compiler: JDK 1.7
+ *Hardware: Intel(R) Celeron(R) 2955U @ 1.40GHz
+ */
+
+/*
+ *This program displays a pretty christmas tree using the console.
+ */
+
+public class Hello
+{
+  // Main: Displays the tree
+  // No preconditions
+  // postcondition: all statements in the main function are executed.
+  public static void main (String[] args)
+  {
+    System.out.println("   *   ");
+    System.out.println("  ***  ");
+    System.out.println(" ***** ");
+    System.out.println("*******");     
+    System.out.println("   *  ");
+    System.out.println("   *  ");
+      
+  }
+}

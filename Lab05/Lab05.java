e53e8bb19f83f120ed5b8d199d75120e1b790d8b
kburts <kevin.burton2@gmail.com>
2014-06-15 14:47:47 -0700

    Finished Lab05 testing on fast pc to see how high a number I can get.

--- Lab05/Lab05.java
+++ Lab05/Lab05.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
+/*Program Name: PalindromePrimes
+ *Author: Kevin
+ *Date: June 16 2014
+ *Course: CPSC1150-1
+ *Instructor: H.Darbandi
+ *Compiler: JDK 1.7
+ *Hardware: Intel(R) Celeron(R) 2955U @ 1.40GHz
+ */
+
 public class Lab05 {
+  // @preconditions: None (menu input later for a number.)
+  // @postconditions: Prints all prime palindromes from 2 - input number.
   public static void main(String[] args) {
     int input = getInput();
     boolean prime = false, palindrome = false;
@@ -12,6 +23,8 @@ public class Lab05 {
   }
 
   public static int getInput() {
+    // Display menu to get an integer input.
+    // returns: integer inputnumber.
     System.out.print("Please choose an integer to be checked for primeness and palindromeness: ");
 
     int inputNumber;
@@ -21,6 +34,8 @@ public class Lab05 {
   }
 
   public static boolean isPrime(int number) {
+    // Check for primeness.
+    // returns: true if prime, or false if not.
     for (int x = 2; x <= number/2; x++) {
       if (number % x == 0)
         return false;
@@ -29,22 +44,16 @@ public class Lab05 {
   }
 
   public static boolean isPalindrome(int number) {
+    // Check for palindromeness.
+    // returns: true if palindrome or false if not.
     if (number == reverseNum(number))
       return true;
     return false;
   }
 
-/**
-  public static int getNumberLength(int number) {
-    int count = 0;
-    while (number > 0) {
-      number = number / 10;
-      count ++;
-    }
-    return count;
-  }
-**/
   public static int reverseNum(int number) {
+    // Reverses input int number.
+    // returns: reversed number.
     int reverse = 0;
     while (number != 0) {
       reverse = reverse * 10;
